3650bbde68d80247263023dc1505aaa77de059ec
lautaromateol <lautaromateol@gmail.com>
2025-01-17 22:11:40 -0300

    add: workspace creation schema

---
+++ features/workspaces/schemas.ts
@@ -0,0 +1,11 @@
+import { z } from "zod"
+
+export const createWorkspaceSchema = z.object({
+  name: z.string({ required_error: "Insert a workspace name." }).trim().min(1, {
+    message: "Insert at least 1 character"
+  }),
+  imageUrl: z.union([
+    z.instanceof(File),
+    z.string().transform((value) => value === "" ? undefined : value)
+  ]).optional()
+})
