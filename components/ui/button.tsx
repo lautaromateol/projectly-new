78d95dfd88730386dc4d9b6578019c37d5bfdac2
lautaromateol <lautaromateol@gmail.com>
2025-01-17 22:12:32 -0300

    update: button variants classes

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -16,8 +16,8 @@ const buttonVariants = cva(
         outline:
           "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
         secondary:
-          "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
+          "bg-rose-100 text-rose-500 shadow-sm hover:bg-rose-100/80",
+        ghost: "hover:bg-rose-100 text-rose-400",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
