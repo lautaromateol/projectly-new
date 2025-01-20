e999487dbac8e844d64e5b23b3c88a300f9420c8
lautaromateol <lautaromateol@gmail.com>
2025-01-20 14:40:45 -0300

    add: nav bar workspace dropdown

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -1,14 +1,16 @@
+import { WorkspaceDropdown } from "@/features/workspaces/components/workspace-dropdown";
 import Image from "next/image";
 
 export function NavBar() {
   return (
-    <nav className="h-16 w-full bg-rose-50/30 flex items-center justify-between px-4 py-6 border-b border-rose-100/80">
+    <header className="h-16 w-full bg-rose-50/30 flex items-center justify-between px-4 py-6 border-b border-rose-100/80">
       <Image
         src="/logo.svg"
         alt="Logo"
-        width={48}
-        height={48}
+        width={40}
+        height={40}
       />
-    </nav>
+     <WorkspaceDropdown />
+    </header>
   )
 }
