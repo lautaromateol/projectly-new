7f44e94f82115d7c6b273ece5bea1ef0c25fd3bb
lautaromateol <lautaromateol@gmail.com>
2025-01-20 14:38:06 -0300

    update: className prop added to workspace avatar

--- features/workspaces/components/workspace-avatar.tsx
+++ features/workspaces/components/workspace-avatar.tsx
@@ -1,20 +1,30 @@
 import Image from "next/image";
+import { cn } from "@/lib/utils";
 
 interface WorkspaceAvatarProps {
   name: string;
   imageUrl?: string;
+  className?: string;
 }
 
-export function WorkspaceAvatar({ name, imageUrl }: WorkspaceAvatarProps) {
+export function WorkspaceAvatar({ name, imageUrl, className }: WorkspaceAvatarProps) {
   if (imageUrl) {
     return (
-      <div className="size-6 rounded-full relative overflow-hidden">
+      <div className={cn(
+        "size-6 rounded-full relative overflow-hidden",
+        className
+      )}>
         <Image src={imageUrl} alt={name} fill />
       </div>
     )
   }
 
   return (
-    <div className="flex items-center justify-center rounded-full bg-rose-700 text-white font-medium size-6">{name.at(0)?.toLowerCase()}</div>
+    <div className={cn(
+      "flex items-center justify-center rounded-full bg-rose-700 text-white font-medium size-6",
+      className
+    )}>
+      {name.at(0)?.toLowerCase()}
+    </div>
   )
 }
