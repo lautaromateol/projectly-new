ecad761c9991e9803c5e0d6f2042091e209cec92
lautaromateol <lautaromateol@gmail.com>
2025-01-20 14:42:59 -0300

    add: update workspace schema

--- features/workspaces/schemas.ts
+++ features/workspaces/schemas.ts
@@ -9,3 +9,13 @@ export const createWorkspaceSchema = z.object({
     z.string().transform((value) => value === "" ? undefined : value)
   ]).optional()
 })
+
+export const updateWorskspaceSchema = z.object({
+  name: z.string({ required_error: "Insert a workspace name." }).trim().min(1, {
+    message: "Insert at least 1 character"
+  }).optional(),
+  imageUrl: z.union([
+    z.instanceof(File),
+    z.string().transform((value) => value === "" ? undefined : value)
+  ]).optional()
+})
