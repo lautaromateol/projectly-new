b88428956d42d2f8912ca70fa9a4f4b35958f584
lautaromateol <lautaromateol@gmail.com>
2025-01-20 14:39:43 -0300

    add: sign out button

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,11 +1,20 @@
 import { Workspaces } from "@/features/workspaces/components/workspaces";
 import { ProfileAvatar } from "./profile-avatar";
+import { SignOutButton } from "@clerk/nextjs";
+import { Button } from "./ui/button";
 
 export function Sidebar() {
   return (
-    <aside className="fixed top-0 left-0 h-screen w-64 bg-rose-50/30 py-6 px-4 border-r border-rose-100/80 space-y-6">
-      <ProfileAvatar />
-      <Workspaces />
+    <aside className="flex flex-col justify-between fixed top-0 left-0 h-screen w-64 bg-rose-50/30 py-6 px-4 border-r border-rose-100/80">
+      <div className="space-y-6">
+        <ProfileAvatar />
+        <Workspaces />
+      </div>
+      <SignOutButton>
+        <Button variant="secondary">
+          Sign Out
+        </Button>
+      </SignOutButton>
     </aside>
   )
 }
