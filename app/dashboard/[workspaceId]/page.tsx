0f6cf7649781f1daa23f4f79d878a8ba7afdcf69
lautaromateol <lautaromateol@gmail.com>
2025-01-20 14:45:19 -0300

    add: workspaceId page validation

--- app/dashboard/[workspaceId]/page.tsx
+++ app/dashboard/[workspaceId]/page.tsx
@@ -1,4 +1,23 @@
-export default function Page() {
+import { prisma } from "@/lib/prisma"
+import { auth } from "@clerk/nextjs/server"
+
+export default async function Page({ params }: { params: { workspaceId: string } }) {
+
+  const { workspaceId } = await params
+
+  const { userId } = await auth()
+
+  const member = await prisma.member.findFirst({
+    where: {
+      userId: userId!,
+      workspaceId
+    }
+  })
+
+  if (!member) {
+    throw new Error("Unauthorized.")
+  }
+
   return (
     <div>Page</div>
   )
