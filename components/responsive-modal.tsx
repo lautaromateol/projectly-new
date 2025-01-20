819c7f798ddf6a883681f5f5228358746627b84e
lautaromateol <lautaromateol@gmail.com>
2025-01-20 14:40:17 -0300

    update: className prop added to responsive modal

--- components/responsive-modal.tsx
+++ components/responsive-modal.tsx
@@ -1,22 +1,24 @@
 import { useIsMobile } from "@/hooks/use-is-mobile"
 import { Drawer, DrawerContent, DrawerHeader, DrawerTitle } from "@/components/ui/drawer"
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import { cn } from "@/lib/utils";
 
 interface ResponsiveModalProps {
   children: React.ReactNode;
   open: boolean;
   onOpenChange: ((open: boolean) => void) | undefined;
   title: string;
+  className?: string;
 }
 
-export function ResponsiveModal({ children, onOpenChange, open, title }: ResponsiveModalProps) {
+export function ResponsiveModal({ children, onOpenChange, open, title, className }: ResponsiveModalProps) {
 
   const isMobile = useIsMobile()
 
   if (isMobile) {
     return (
       <Drawer open={open} onOpenChange={onOpenChange}>
-        <DrawerContent>
+        <DrawerContent className={cn(className)}>
           <DrawerHeader>
             <DrawerTitle>{title}</DrawerTitle>
           </DrawerHeader>
@@ -28,7 +30,7 @@ export function ResponsiveModal({ children, onOpenChange, open, title }: Respons
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent>
+      <DialogContent className={cn(className)}>
         <DialogHeader>
           <DialogTitle>{title}</DialogTitle>
         </DialogHeader>
