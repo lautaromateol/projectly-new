639731d1048c4c7b3ce84ffc09c12ca89a368093
lautaromateol <lautaromateol@gmail.com>
2025-01-21 16:59:57 -0300

    update: project creation form modal wrapper added

--- providers/modals-provider.tsx
+++ providers/modals-provider.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react"
 import { WorkspaceMembersWrapper } from "@/features/members/components/workspace-members-wrapper"
 import { WorkspaceSettingsWrapper } from "@/features/settings/components/workspace-settings-wrapper"
 import { CreateWorkspaceFormWrapper } from "@/features/workspaces/components/create-workspace-form-wrapper"
+import { CreateProjectFormWrapper } from "@/features/projects/components/create-project-form-wrapper"
 
 export function ModalsProvider() {
 
@@ -18,6 +19,7 @@ export function ModalsProvider() {
         <CreateWorkspaceFormWrapper />
         <WorkspaceSettingsWrapper />
         <WorkspaceMembersWrapper />
+        <CreateProjectFormWrapper />
       </>
     )
   }
