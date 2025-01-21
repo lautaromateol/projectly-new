cce2ac8951849b20b020c7e2b5d94fca4a644863
lautaromateol <lautaromateol@gmail.com>
2025-01-21 12:24:23 -0300

    update: mobile sidebar trigger added

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -1,16 +1,20 @@
-import { WorkspaceDropdown } from "@/features/workspaces/components/workspace-dropdown";
 import Image from "next/image";
+import { WorkspaceDropdown } from "@/features/workspaces/components/workspace-dropdown";
+import { MobileSidebar } from "./mobile-sidebar";
 
 export function NavBar() {
   return (
     <header className="h-16 w-full bg-rose-50/30 flex items-center justify-between px-4 py-6 border-b border-rose-100/80">
-      <Image
-        src="/logo.svg"
-        alt="Logo"
-        width={40}
-        height={40}
-      />
-     <WorkspaceDropdown />
+      <div className="flex items-center gap-x-2">
+        <Image
+          src="/logo.svg"
+          alt="Logo"
+          width={40}
+          height={40}
+        />
+        <MobileSidebar />
+      </div>
+      <WorkspaceDropdown />
     </header>
   )
 }
