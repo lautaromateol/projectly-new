03a30ef011d45862dab0c668d4680f36a81c9b94
lautaromateol <lautaromateol@gmail.com>
2025-01-21 20:02:10 -0300

    update: project switcher hover and params effect

--- features/projects/components/projects-switcher.tsx
+++ features/projects/components/projects-switcher.tsx
@@ -2,8 +2,10 @@ import Link from "next/link";
 import { Loader2 } from "lucide-react";
 import type { Dispatch, SetStateAction } from "react";
 import { useGetProjects } from "@/features/projects/api/use-get-projects";
-import { WorkspaceAvatar } from "@/features/workspaces/components/workspace-avatar";
+import { useProjectId } from "@/features/projects/hooks/use-project-id";
 import { Collapsible, CollapsibleContent } from "@/components/ui/collapsible";
+import { ProjectAvatar } from "./project-avatar";
+import { cn } from "@/lib/utils";
 
 interface ProjectsSwitcherProps {
   isOpen: boolean;
@@ -14,6 +16,8 @@ export function ProjectsSwitcher({ isOpen, setIsOpen }: ProjectsSwitcherProps) {
 
   const { projects, isLoadingProjects } = useGetProjects()
 
+  const projectId = useProjectId()
+
   if (isLoadingProjects) {
     return (
       <Loader2 className="size-8 animate-spin text-rose-600 mb-4 mx-auto" />
@@ -22,11 +26,14 @@ export function ProjectsSwitcher({ isOpen, setIsOpen }: ProjectsSwitcherProps) {
 
   return (
     <Collapsible open={isOpen} onOpenChange={setIsOpen}>
-      <CollapsibleContent className="space-y-0.5 mb-2">
+      <CollapsibleContent className="flex flex-col gap-y-1.5 mb-2">
         {projects?.map((project) => (
           <Link href={`/dashboard/${project.workspaceId}/projects/${project.id}`} key={project.id}>
-            <div className="flex items-center gap-x-2 px-2 py-1 rounded-lg">
-              <WorkspaceAvatar
+            <div className={cn(
+              "flex items-center gap-x-2 p-2 rounded-lg hover:shadow hover:bg-rose-100/50",
+              projectId === project.id ? "shadow bg-rose-100/50" : ""
+            )}>
+              <ProjectAvatar
                 name={project.name}
                 imageUrl={project.imageUrl ?? ""}
               />
