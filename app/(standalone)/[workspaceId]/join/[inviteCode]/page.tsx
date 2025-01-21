6aa6cd7300394e1e0e8d75251c80ec63f2d504df
lautaromateol <lautaromateol@gmail.com>
2025-01-21 00:13:31 -0300

    fix

--- app/(standalone)/[workspaceId]/join/[inviteCode]/page.tsx
+++ app/(standalone)/[workspaceId]/join/[inviteCode]/page.tsx
@@ -1,21 +1,15 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { notFound } from "next/navigation"
 import { prisma } from "@/lib/prisma"
 import { JoinWorkspace } from "./components/join-workspace"
 
-interface JoinWorkspacePageProps {
-  params: {
-    workspaceId: string
-  }
-}
+export default async function JoinWorkspacePage({ params }: any) {
 
-
-export default async function JoinWorkspacePage({ params }: JoinWorkspacePageProps) {
-
-  const { workspaceId } = params
+  const { workspaceId } = await params
 
   const workspace = await prisma.workspace.findFirst({
     where: {
-      id: workspaceId
+      id: workspaceId as string
     }
   })
 
