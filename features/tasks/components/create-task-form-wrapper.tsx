59946a0f78bac10a9202ef475f97bdb25e2ff2e5
lautaromateol <lautaromateol@gmail.com>
2025-01-22 21:59:42 -0300

    add: task creation form

---
+++ features/tasks/components/create-task-form-wrapper.tsx
@@ -0,0 +1,14 @@
+import { useCreateTaskModal } from "@/features/tasks/hooks/use-create-project-modal";
+import { ResponsiveModal } from "@/components/responsive-modal";
+import { CreateTaskForm } from "./create-task-form";
+
+export function CreateTaskFormWrapper() {
+
+  const { isOpen, close } = useCreateTaskModal()
+
+  return (
+    <ResponsiveModal title="Create a new Task" open={isOpen} onOpenChange={close}>
+      <CreateTaskForm />
+    </ResponsiveModal>
+  )
+}
