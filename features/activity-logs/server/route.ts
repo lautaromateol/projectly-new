de92fd6ff25b5824e3af47184827d6df836e726b
lautaromateol <lautaromateol@gmail.com>
2025-01-23 22:16:47 -0300

    add: activity logs route

---
+++ features/activity-logs/server/route.ts
@@ -0,0 +1,37 @@
+import { prisma } from "@/lib/prisma";
+import { sessionMiddleware } from "@/lib/session-middleware";
+import { Hono } from "hono";
+
+const app = new Hono()
+  .use("*", sessionMiddleware)
+  .get("/:taskId",
+    async (c) => {
+      const { taskId } = c.req.param()
+
+      const activityLogs = await prisma.activityLog.findMany({
+        where: {
+          taskId
+        },
+        include: {
+          member: {
+            select: {
+              name: true,
+              imageUrl: true
+            }
+          },
+          task: {
+            select: {
+              name: true
+            }
+          }
+        },
+        orderBy: {
+          createdAt: "desc"
+        }
+      })
+
+      return c.json({ data: activityLogs }, 200)
+    }
+  )
+
+export default app
