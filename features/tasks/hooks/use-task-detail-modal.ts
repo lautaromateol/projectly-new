3bf05f83d6574074f0d32ee3c7da419092c9ff93
lautaromateol <lautaromateol@gmail.com>
2025-01-24 21:15:33 -0300

    update: id property added to optimize use

--- features/tasks/hooks/use-task-detail-modal.ts
+++ features/tasks/hooks/use-task-detail-modal.ts
@@ -1,13 +1,15 @@
 import { create } from "zustand"
 
 interface UseTaskDetailModalProps {
+  id?: string;
   isOpen: boolean;
-  open: () => void;
+  open: (id: string) => void;
   close: () => void;
 }
 
 export const useTaskDetailModal = create<UseTaskDetailModalProps>((set) => ({
+  id: undefined,
   isOpen: false,
-  open: () => set({ isOpen: true }),
-  close: () => set({ isOpen: false })
+  open: (id: string) => set({ isOpen: true, id }),
+  close: () => set({ isOpen: false, id: undefined })
 }))
