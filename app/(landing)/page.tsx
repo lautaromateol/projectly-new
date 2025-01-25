7b0797ed30c7797c307b46394a23016eb1f0fd39
lautaromateol <lautaromateol@gmail.com>
2025-01-25 13:21:32 -0300

    add: landing page

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,11 @@
+import { HomeFeatures } from "@/components/landing/home-features";
+import { HomeHero } from "@/components/landing/home-hero";
+
+export default function LandingPage() {
+  return (
+    <>
+      <HomeHero />
+      <HomeFeatures />
+    </>
+  );
+}
